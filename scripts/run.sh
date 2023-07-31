f829af7bc8e2cc0efb47a11e0c3adcee195b53c9
Huaqi Fang <578567190@qq.com>
2023-07-31 15:08:26 +0800

    script: set LD_LIBRARY_PATH when run program to search correct library
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,7 @@
 BUILDDIR=${1:-vp.build}
 
 export SC_SIGNAL_WRITE_CHECK=DISABLE
+# set extra library load path
+export LD_LIBRARY_PATH=$(pwd)/$BUILDDIR/install/lib:$LD_LIBRARY_PATH
+
 ./$BUILDDIR/install/bin/riscv64_toplevel -c conf/riscv64.lua
