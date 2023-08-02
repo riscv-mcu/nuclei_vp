8dd279b6fdc092ee5902f456ec528de6fc76f3f8
Huaqi Fang <578567190@qq.com>
2023-08-02 11:20:35 +0800

    build: mark build failed when systemc library build fail
    - Add JOBS and V environment variable to control build in parallel jobs
      and verbose build message
    
    such as
    
    V=1 JOBS=2 bash scripts/build.sh vp.buildv2
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 BUILDDIR=${1:-vp.build}
+V=${V:-0}
+JOBS=${JOBS:-8}
 
 mkdir -p $BUILDDIR
+
+if [ "x$V" == "x1" ] ; then
+    export VERBOSE=1
+fi
 
 BUILDDIR=$(realpath $BUILDDIR)
 
@@ -11,14 +17,26 @@ VPSRCDIR=$(realpath $SCRIPTDIR/..)
 
 if [ -d $VPSRCDIR/libs/systemc ] ; then
     syscbuild=$BUILDDIR/libs/systemc.build
+    systemcver=$(head -1 $VPSRCDIR/libs/systemc/RELEASENOTES | sed 's/.*SystemC //')
     if [ ! -f $syscbuild/STAMP ] ; then
-        echo "Build and install systemc library"
+        echo "Build and install systemc $systemcver library"
         mkdir -p $syscbuild/install
         pushd $syscbuild
-        $VPSRCDIR/libs/systemc/configure --prefix=$syscbuild/install
+        if [ -f $VPSRCDIR/libs/systemc/cmake/INSTALL_USING_CMAKE ] ; then
+            echo "SystemC $systemcver configured using cmake"
+            cmake -DCMAKE_INSTALL_PREFIX=$syscbuild/install $VPSRCDIR/libs/systemc/
+        else
+            echo "SystemC $systemcver configured using auto tools"
+            $VPSRCDIR/libs/systemc/configure --prefix=$syscbuild/install
+        fi
         make -j
         make -j install
-        touch $syscbuild/STAMP
+        if [ "x$?" == "x0" ] ; then
+            echo $systemcver > $syscbuild/STAMP
+        else
+            echo "Failed to build systemc library"
+            exit 1
+        fi
         popd
     else
         echo "SystemC and TLM library is built already, no need to rebuild it"
@@ -33,6 +51,6 @@ if [ ! -f $BUILDDIR/Makefile ] ; then
 else
     echo "Project is already configured!"
 fi
-make | tee make.log
+make -j$JOBS | tee make.log
 make install
 popd
