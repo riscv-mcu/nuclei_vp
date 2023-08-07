cd06471d3dcf1f140bde5923c537229f7a4c0af7
Huaqi Fang <578567190@qq.com>
2023-08-07 18:08:59 +0800

    add conf/riscv64_eclic.lua conf to run with eclic
    ./scripts/run.sh vp.build conf/riscv64_eclic.lua
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

---
+++ conf/riscv64_eclic.lua
@@ -0,0 +1,14 @@
+CPU = {
+    library = "libqbox-riscv64-softmmu.so",
+    extra_arguments = "-machine nuclei_evalsoc,download=flashxip -cpu nuclei-nx900fd -nographic -smp 1 -m 1024m  -kernel images/demo_eclic.elf -nographic -nodefaults -serial stdio "
+}
+
+ram = {
+    size = 2097152,
+    target_port = {
+        base_addr = 0x80000000,
+        high_addr = 0xffffffff
+    }
+}
+
+
