00e9288e0af9a2590db079414064f3b1b34ee463
Huaqi Fang <578567190@qq.com>
2023-08-07 18:09:51 +0800

    add new variable to support different configuration file as variable 2
    see
    
    ./scripts/run.sh vp.build conf/riscv64_eclic.lua
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 BUILDDIR=${1:-vp.build}
+CONF=${2:-conf/riscv64.lua}
 
 export SC_SIGNAL_WRITE_CHECK=DISABLE
 # set extra library load path
 export LD_LIBRARY_PATH=$(pwd)/$BUILDDIR/install/lib:$LD_LIBRARY_PATH
 
-./$BUILDDIR/install/bin/riscv64_toplevel -c conf/riscv64.lua
+if [ ! -f $CONF ] ; then
+    echo "Configuration file $CONF not exist, please check!"
+    exit 1
+fi
+echo "Using configuration file $CONF"
+./$BUILDDIR/install/bin/riscv64_toplevel -c $CONF
