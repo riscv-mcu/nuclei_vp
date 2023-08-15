05cffca4b582c04df5f8ff8db02d9c1f2bc9174f
Huaqi Fang <578567190@qq.com>
2023-08-15 11:23:23 +0800

    scripts: accept extra args when do run.sh
    eg: ./scripts/run.sh vp.build_8.0 conf/riscv64.lua --dmi_disable
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,10 @@
 BUILDDIR=${1:-vp.build}
 CONF=${2:-conf/riscv64.lua}
 
+# get remaining arguments
+shift 2
+RESTARGS=$@
+
 export SC_SIGNAL_WRITE_CHECK=DISABLE
 # set extra library load path
 export LD_LIBRARY_PATH=$(pwd)/$BUILDDIR/install/lib:$LD_LIBRARY_PATH
@@ -11,4 +15,4 @@ if [ ! -f $CONF ] ; then
     exit 1
 fi
 echo "Using configuration file $CONF"
-./$BUILDDIR/install/bin/riscv_toplevel -c $CONF
+./$BUILDDIR/install/bin/riscv_toplevel -c $CONF $RESTARGS
